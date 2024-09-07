cee540ccda2e092434cfc95e26aaee8a28018773
ashupednekar <ashutoshpednekar15@gmail.com>
2024-09-07 12:50:04 +0530

    fix: added intoresponse trait

---
+++ src/extras/response.rs
@@ -0,0 +1,15 @@
+use crate::StandardError;
+use axum::response::IntoResponse;
+use axum::Json;
+use axum::response::Response;
+use serde_json::json;
+
+impl IntoResponse for StandardError {
+    fn into_response(self) -> Response {
+        (
+            self.status_code,
+            Json(json!({"detail": self.message}))
+        )
+            .into_response()
+    }
+}
