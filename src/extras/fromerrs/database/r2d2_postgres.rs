539b1d8d98aa6ca31191b447687ee6b7beba57eb
ashupednekar <ashutoshpednekar15@gmail.com>
2024-09-11 13:59:46 +0530

    fix: updated db feature to properly include diesel and r2d2-postgres from traits

--- src/extras/fromerrs/database/r2d2_postgres.rs
+++ src/extras/fromerrs/database/r2d2_postgres.rs
@@ -1,6 +1,9 @@
 use crate::{StandardError, Interpolate};
+#[cfg(feature="diesel")]
 use r2d2_postgres::r2d2::Error; 
 
+
+#[cfg(feature="diesel")]
 impl From<Error> for StandardError {
     fn from(error: Error) -> Self {
         StandardError::new("ER-DB-POOL").interpolate_err(error.to_string())
