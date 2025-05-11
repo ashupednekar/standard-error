d949d10bde7f410410448634c945128587fed55d
ashupednekar <ashutoshpednekar15@gmail.com>
2025-05-11 11:43:59 +0530

    chore: updated feature in aksama from err

--- src/extras/htmlres.rs
+++ src/extras/htmlres.rs
@@ -1,9 +1,12 @@
+#[cfg(feature = "askama")]
 use crate::StandardError;
 
+#[cfg(feature = "askama")]
 pub trait HtmlRes {
     fn template(&mut self, t: String) -> Self;
 }
 
+#[cfg(feature = "askama")]
 impl HtmlRes for StandardError {
     fn template(&mut self, t: String) -> Self {
         self.html = Some(t);
