4a4accded0bdb4fd72d2be5bd0536ac1aad4143f
ashupednekar <ashutoshpednekar15@gmail.com>
2025-05-11 12:09:16 +0530

    fix: html resopnse

--- src/extras/response.rs
+++ src/extras/response.rs
@@ -1,6 +1,5 @@
 use crate::StandardError;
-use axum::response::IntoResponse;
-use axum::response::Response;
+use axum::response::{Response, IntoResponse, Html};
 use axum::Json;
 use serde_json::json;
 
@@ -8,7 +7,7 @@ impl IntoResponse for StandardError {
     fn into_response(self) -> Response {
         #[cfg(feature = "askama")]
         if let Some(html) = self.html {
-            return (self.status_code, html).into_response();
+            return (self.status_code, Html(html)).into_response();
         }
         (self.status_code, Json(json!({"detail": self.message}))).into_response()
     }
